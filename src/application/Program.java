d027affa85bd3fa925e4426b512c9fa2417a3403
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-07-22 20:07:22 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args){
+
+        Position ps = new Position(3,6);
+        System.out.println(ps);
 
     }
 }
