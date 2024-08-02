614e91e3004bd569ca6058d84e995e575c5d3855
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-08-01 22:27:59 -0300

    Check Logic part-2

--- src/application/Program.java
+++ src/application/Program.java
@@ -44,7 +44,6 @@ public class Program {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
-
         }
     }
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,6 +9,8 @@ import chess.ChessException;
 import chess.ChessPosition;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class ChessMatch {
 
@@ -37,6 +39,10 @@ public class ChessMatch {
         return currentPlayer;
     }
 
+    public boolean getCheck(){
+        return check;
+    }
+
     public ChessPiece[][] getPieces(){
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 
@@ -63,6 +69,14 @@ public class ChessMatch {
         validateSourcePosition(source);
         validTargetPosition(source,target);
         Piece capturedPiece = makeMove(source, target);
+
+        if(testCheck(currentPlayer)){
+            undoMove(source,target, capturedPiece);
+            throw new ChessException("You can't put yourself in check");
+        }
+
+        check = (testCheck(opponent(currentPlayer))) ? true : false;
+
         nextTurn();
         return (ChessPiece) capturedPiece;
     }
@@ -127,6 +141,18 @@ public class ChessMatch {
             }
         }
         throw new IllegalArgumentException("There is no " + color + "king on the board");
+    }
+
+    private boolean testCheck(Color color){
+        Position kingPosition = king(color).getChessPosition().toPosition();
+        List<Piece> opponentPieces = piecesOnTheBoard.stream().filter(x-> ((ChessPiece)x).getColor() == opponent(color)).collect(Collectors.toList());
+        for(Piece p : opponentPieces){
+            boolean[][] mat = p.possibleMoves();
+            if(mat[kingPosition.getRow()][kingPosition.getColumn()]){
+                return true;
+            }
+        }
+        return false;
     }
     private void placeNewPiece(char column, int row, ChessPiece piece) {
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
